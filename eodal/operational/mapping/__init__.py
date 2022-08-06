23debf23c2e18a18e182912495329205ea1dd9ca
lukas <lukasvalentin.graf@usys.ethz.ch>
2022-08-06 10:46:09 +0200

    updated import statements

--- eodal/operational/mapping/__init__.py
+++ eodal/operational/mapping/__init__.py
@@ -1,3 +1,5 @@
 from eodal.operational.mapping.sentinel2 import Sentinel2Mapper
 from eodal.operational.mapping.mapper import MapperConfigs
 from eodal.operational.mapping.mapper import Mapper
+
+from eodal.operational.mapping.utils import plot_feature
