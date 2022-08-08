40ebbfd03738dc2c54312bf83c028bb2f91a969d
lukas <graflu@ethz.ch>
2022-08-08 17:00:02 +0200

    moved functionalities to STAC client module

--- eodal/core/sensors/sentinel1.py
+++ eodal/core/sensors/sentinel1.py
@@ -33,64 +33,14 @@ class Sentinel1_RTC(Sentinel1):
     """
 
     @classmethod
-    def from_ms_pc(
+    def from_safe(
             cls,
-            sensing_date: date,
             polarizations: Optional[str] = ['VV', 'VH'],
             **kwargs
         ):
         """
         """
-        # a bounding box (vector features) is required
-        vector_features = kwargs.get('vector_features', None)
-        if 'vector_features' is None:
-            raise ValueError('A bounding box (vector_features) must be specified')
-        # check STAC provider and status
-        if not Settings.USE_STAC:
-            raise ValueError('This method requires STAC')
-        if Settings.STAC_BACKEND != STAC_Providers.MSPC:
-            raise ValueError('This method requires Microsoft Planetary Computer')
-        if Settings.PC_SDK_SUBSCRIPTION_KEY == '':
-            raise ValueError('This method requires a valid Planetary Computer API key')
-
-        # query the catalog
-        if isinstance(vector_features, Path):
-            vector_features = gpd.read_file(vector_features)
-        # construct the bounding box from vector features
-        # the bbox must be provided as a polygon in geographic coordinates
-        vector_features.to_crs(epsg=4326, inplace=True)
-        bbox = vector_features.total_bounds
-        catalog = pystac_client.Client.open(
-            "https://planetarycomputer.microsoft.com/api/stac/v1"
-        )
-        search = catalog.search(
-            collections=["sentinel-1-rtc"], bbox=bbox , datetime=sensing_date.strftime('%Y-%m-%d')
-        )
-        items = search.get_all_items()
-        # it might happen that no item was found for a given date. In this case
-        # re-do the search without the datetime filter and return the item that is
-        # closest to the sensing_date passed
-        if len(items) == 0:
-            search = catalog.search(
-                collections=["sentinel-1-rtc"], bbox=bbox
-            )
-            items = search.get_all_items()
-            time_diffs = []
-            for item in items:
-                item_date = datetime.strptime(item.properties['datetime'], '%Y-%m-%dT%H:%M:%S.%fZ').date()
-                time_diff = item_date - sensing_date
-                # save the absolute time difference, otherwise argmin makes no sense
-                time_diffs.append(abs(time_diff))
-            time_diff_argmin = np.argmin(time_diffs)
-            items = items[time_diff_argmin]
-
-        # sign requests (required to have read access to the data)
-        if not isinstance(items, ItemCollection):
-            items = [items]
-        signed_items = []
-        for item in items:
-            signed_items.append(planetary_computer.sign_item(item))
-        # TODO: read polarization (files) and properties -> save into own class as for S2
+        pass
 
             
             
