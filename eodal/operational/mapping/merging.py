fb7637f059f77da5b0de5a4a5c3232b634e3c222
lukas <graflu@ethz.ch>
2022-12-07 12:37:13 +0100

    marked future to-do

--- eodal/operational/mapping/merging.py
+++ eodal/operational/mapping/merging.py
@@ -17,6 +17,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
+# TODO: move this to eodal algorithms
+
 import os
 import geopandas as gpd
 import uuid
