5fa41857f1fdef837016d4810cfdfcddc60c289a
lukas <lukasvalentin.graf@usys.ethz.ch>
2023-02-20 08:48:59 +0100

    removed `operational` submodule in EOdal

--- eodal/operational/cli/__init__.py
+++
@@ -1,4 +0,0 @@
-from .sentinel2 import cli_s2_pipeline_fun
-from .sentinel2 import cli_s2_creodias_update
-from .sentinel2 import cli_s2_scene_selection
-from .sentinel2 import cli_s2_sen2cor_update
