1d3ccb803fe1e542d17feeb2913900bbebe9a9f0
lukas <lukasvalentin.graf@usys.ethz.ch>
2023-02-20 09:43:47 +0100

    updated script to latest version of EOdal mapper and moved to scripts

--- scripts/sentinel2_mapper.py
+++ scripts/sentinel2_mapper.py
@@ -1,29 +1,24 @@
 """
-Example script to extract a time series of Sentinel-2 mapper for a
+Script to extract a collection of Sentinel-2 scenes for a
 custom area of interest (AOI).
 
-The script shows how to use the Sentinel2Mapper class that takes over
+The script shows how to use the EOdal Mapper class that takes over
 data handling such as
 
 	* querying of spatio-temporal metadata catalogs to identify
-	  available Sentinel-2 mapper
+	  available Sentinel-2 scenes
 	* merging data from different Sentinel-2 tiles if required
 	* re-projection of imagery from one UTM zone into another
 	  if required
-	* removal of black-filled mapper
+	* removal of black-filled (i.e., no-data) scenes
 
-This script works either using local data sources or by retrieving Sentinel-2
-imagery from Microsoft Planetary Computer (https://planetarycomputer.microsoft.com).
-No authentication required.
+This script works by retrieving Sentinel-2 scenes from
+Microsoft Planetary Computer (https://planetarycomputer.microsoft.com).
+This requires no authentication required. Alternatively, the same code
+can be used to read data from a local EOdal Sentinel archive or a different
+STAC provider.
 
-To use Planetary Computer make sure to set the `USE_STAC` variable to True
-and specify Microsoft as STAC provider (default).
-
-.. code-block:: shell
-
-    export USE_STAC = "True"
-
-Copyright (C) 2022 Lukas Valentin Graf
+Copyright (C) 2022/23 Lukas Valentin Graf
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -38,71 +33,89 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-import cv2
 
-from datetime import date
+import geopandas as gpd
+
+from datetime import datetime
 from eodal.config import get_settings
-from eodal.operational.mapping import MapperConfigs, Sentinel2Mapper
-from eodal.operational.mapping import plot_feature
-from eodal.utils.sentinel2 import ProcessingLevels
+from eodal.core.sensors.sentinel2 import Sentinel2
+from eodal.mapper.feature import Feature
+from eodal.mapper.filter import Filter
+from eodal.mapper.mapper import Mapper, MapperConfigs
+
 from pathlib import Path
+from typing import List
 
 Settings = get_settings()
+# set to False to use a local data archove
 Settings.USE_STAC = True
 
-#%% user-inputs
-# ---------------------- Spatial Resolution ----------------------------
-spatial_resolution: int = 10 			# meters
-resampling_method: int = cv2.INTER_NEAREST_EXACT
+def preprocess_sentinel2_scenes(
+		ds: Sentinel2,
+		target_resoltion: int,
+	) -> Sentinel2:
+	"""
+	Resample Sentinel-2 scenes and mask clouds, shadows, and snow
+	based on the Scene Classification Layer (SCL)
 
-# ----------------------- Processing Level -----------------------------
-processing_level: ProcessingLevels = ProcessingLevels.L2A 	# BOA
+	:param target_resolution:
+		spatial target resolution to resample all bands to
+	:returns:
+		resampled, cloud-masked Sentinel-2 scene
+	"""
+	# resample scene
+	ds.resample(inplace=True, target_resoltion=target_resoltion)
+	# mask clouds, shadows, and snow
+	return ds.mask_clouds_and_shadows()
 
-# ------------------------ Cloud Coverage ------------------------------
-scene_cloud_cover_threshold: int = 50 	# percent
-
-# ------------------------- Time Range ---------------------------------
-date_start: date = date(2022,4,1)  		# year, month, day (incl.)
-date_end: date = date(2022,4,14)   		# year, month, day (incl.)
-
-# ---------------------- Area of Interest ------------------------------
-aoi: Path = Path('../data/sample_polygons/lake_lucerne.gpkg')
-
-#%% executable part
-# Sentinel-2 mapper configuration
-mapper_configs = MapperConfigs(
-	spatial_resolution=spatial_resolution,
-	resampling_method=resampling_method,
-)
-
-# get a new mapper instance
-mapper = Sentinel2Mapper(
-	date_start=date_start,
-	date_end=date_end,
-	processing_level=processing_level,
-	cloud_cover_threshold=scene_cloud_cover_threshold,
-	mapper_configs=mapper_configs,
-	feature_collection=aoi
-)
-
-# retrieve metadata of mapper found (no reading)
-mapper.get_scenes()
-mapper.observations # displays mapper found
-
-# read data into eodal's RasterCollection objects
-s2_data = mapper.get_complete_timeseries()
-features = mapper.get_feature_ids()
-
-# loop features (in this case it's just a single one) and plot them
-for feature in features:
-	fig_feature = plot_feature(
-		feature_scenes=s2_data[feature],
-		band_selection=['nir_1', 'red', 'green'],
-		figsize=(16,14),
-		max_scenes_in_row=2,
-		sharex=True,
-		sharey=True
+if __name__ == '__main__':
+	#%% user-inputs
+	# -------------------------- Collection -------------------------------
+	collection: str = 'sentinel2-msi'
+	
+	# ------------------------- Time Range ---------------------------------
+	time_start: datetime = datetime(2022,4,1)  		# year, month, day (incl.)
+	time_end: datetime = datetime(2022,4,14)   		# year, month, day (incl.)
+	
+	# ---------------------- Spatial Feature  ------------------------------
+	geom: Path = Path('../data/sample_polygons/lake_lucerne.gpkg')
+	
+	# ------------------------- Metadata Filters ---------------------------
+	metadata_filters: List[Filter] = [
+		Filter('cloudy_pixel_percentage','<', 80),
+		Filter('processing_level', '==', 'Level-2A')
+	]
+	
+	#%% query the scenes available (no I/O of scenes, this only fetches metadata)
+	feature = Feature.from_geoseries(gpd.read_file(geom).geometry)
+	mapper_configs = MapperConfigs(
+	    collection=collection,
+	    time_start=time_start,
+	    time_end=time_end,
+	    feature=feature,
+	    metadata_filters=metadata_filters
 	)
-	fig_feature.savefig('../img/s2_lake_lucerne.png', bbox_inches='tight')
+	# to enhance reproducibility and provide proper documentation, the MapperConfigs
+	# can be saved as yaml (and also then be loaded again from yaml)
+	mapper_configs.to_yaml('../data/sample_mapper_call.yaml')
+	
+	# now, a new Mapper instance is created
+	mapper = Mapper(mapper_configs, sensor='sentinel2')
+	mapper.query_scenes()
+	# the metadata is loaded into a GeoPandas GeoDataFrame
+	mapper.metadata
+	
+	#%% load the scenes available from STAC
+	scene_kwargs = {
+	    'scene_constructor': Sentinel2.from_safe,
+	    'scene_constructor_kwargs': {'band_selection': ['B04', 'B08']},
+	    'scene_modifier': preprocess_sentinel2_scenes,
+	    'scene_modifier_kwargs': {'target_resolution': 10}
+	}
+	mapper.load_scenes(scene_kwargs=scene_kwargs)
+	# the data loaded into `mapper.data` as a EOdal SceneCollection
+	mapper.data
 
-# further program logic ...
+	# EOdal SceneCollections can be made persistent by storing them as serialized pickled
+	# objects on disk (and can be loaded from there)
+	
