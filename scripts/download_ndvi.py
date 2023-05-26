e0ef21767e98d3958cec0b2bcbb9a1e0a011fd50
lukas <lukasvalentin.graf@usys.ethz.ch>
2023-05-26 10:00:32 +0200

    sample script for NDVI calculation

---
+++ scripts/download_ndvi.py
@@ -0,0 +1,101 @@
+"""
+Script to get NDVI from Sentinel-2 for a specific area of interest
+(e.g., a field parcel) for a pre-defined time period.
+
+@author: Lukas Valentin Graf
+"""
+
+import geopandas as gpd
+import planetary_computer
+import urllib
+
+from datetime import datetime
+from eodal.config import get_settings
+from eodal.core.raster import RasterCollection
+from eodal.core.scene import SceneCollection
+from eodal.core.sensors.sentinel2 import Sentinel2
+from eodal.mapper.feature import Feature
+from eodal.mapper.filter import Filter
+from eodal.mapper.mapper import Mapper, MapperConfigs
+
+from pathlib import Path
+from typing import List
+
+
+Settings = get_settings()
+# set to False to use a local data archove
+Settings.USE_STAC = True
+
+
+def get_ndvi(
+    mapper: Mapper,
+    output_dir: Path
+) -> None:
+    """
+    Fetch Sentinel-2 scenes, calculate the NDVI and
+    save the NDVI as geoTiff files.
+
+    :param mapper:
+        eodal Mapper instance
+    :param output_dir:
+        directory where to save the GeoTiff files to
+    """
+    mapper.query_scenes()
+
+    # load the scenes available from STAC
+    scene_kwargs = {
+        'scene_constructor': Sentinel2.from_safe,
+        'scene_constructor_kwargs': {
+            'band_selection': ["B04", "B08"]}}
+    mapper.load_scenes(scene_kwargs=scene_kwargs)
+
+    # calculate the NDVI
+    for scene_uri, scene in mapper.data:
+        scene.calc_si('NDVI', inplace=True)
+        # save NDVI as GeoTiff
+        fpath_ndvi = ouput_dir.joinpath(
+            scene_uri.stem + '_ndvi.tif'
+        )
+        scene['ndvi'].to_rasterio(fpath_ndvi)
+
+
+if __name__ == '__main__':
+
+    # -------------------------- Paths -------------------------------------
+    # define the output directory where to save the NDVI GeoTiff files
+    out_dir = Path('./data')
+
+    # user-inputs
+    # -------------------------- Collection -------------------------------
+    collection: str = 'sentinel2-msi'
+
+    # ------------------------- Time Range ---------------------------------
+    time_start: datetime = datetime(2022, 6, 1)  		# year, month, day (incl.)
+    time_end: datetime = datetime(2022, 6, 30)   		# year, month, day (incl.)
+
+    # ----------------------- Cloudy Pixel Percentage ----------------------
+    cloudy_pixel_percentage: int = 25  # percent (scene-wide)
+
+    # ---------------------- Spatial Feature  ------------------------------
+    geom = \
+        'https://raw.githubusercontent.com/neffjulian/remote_sensing/main/images/coordinates/zhr_coordinates.geojson'  # noqa: E501
+    # spatial features can be single field or multiple field parcels as geojson, shapefile, etc.
+
+    # ------------------------- Metadata Filters ---------------------------
+    metadata_filters: List[Filter] = [
+        Filter('cloudy_pixel_percentage', '<=', cloudy_pixel_percentage),
+        Filter('processing_level', '==', 'Level-2A')]
+
+    # query the scenes available (no I/O of scenes, this only fetches metadata)
+    feature = Feature.from_geoseries(gpd.read_file(geom).geometry.dissolve())
+    mapper_configs = MapperConfigs(
+        collection=collection,
+        time_start=time_start,
+        time_end=time_end,
+        feature=feature,
+        metadata_filters=metadata_filters)
+
+    # now, a new Mapper instance is created
+    mapper = Mapper(mapper_configs)
+
+    get_ndvi(mapper, output_dir)
