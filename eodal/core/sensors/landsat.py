4a9adb502be0300960043805239f9d948ff5b665
lukas <lukasvalentin.graf@usys.ethz.ch>
2023-05-30 18:19:50 +0200

    first draft of `from_usgs` method

--- eodal/core/sensors/landsat.py
+++ eodal/core/sensors/landsat.py
@@ -3,7 +3,7 @@ This module contains the ``Landsat`` class that inherits from
 eodal's core ``RasterCollection`` class.
 
 The ``Landsat`` class enables reading one or more spectral bands from Landsat 5 to 9
-science products in L2 processing level. 
+science products in L2 processing level.
 
 The class handles data in L1C and L2A processing level.
 
@@ -25,15 +25,19 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import annotations
 
+import cv2
 import pandas as pd
 import planetary_computer
 
+from datetime import datetime
 from pathlib import Path
 from typing import Dict, List, Optional
 
 from eodal.config import get_settings, STAC_Providers
-from eodal.core.raster import RasterCollection
+from eodal.core.band import Band
+from eodal.core.raster import RasterCollection, SceneProperties
 from eodal.utils.geometry import adopt_vector_features_to_mask
+from eodal.utils.constants import ProcessingLevels
 from eodal.utils.constants.landsat import (
     band_resolution,
     landsat_band_mapping,
@@ -91,6 +95,52 @@ class Landsat(RasterCollection):
         # get the platform from the band name
         platform = test_band.split('_')[0]
         return platform
+
+    @staticmethod
+    def _product_uri_from_filename(
+            in_dir: Path | Dict[str, str]
+    ) -> str:
+        """
+        Get the product URI from the file name.
+
+        :param in_dir:
+            Path to the directory containing the Landsat data or collection of
+            hyper-links in the case of STAC. If a dictionary is passed, we assume
+            that all files are stored in the same directory.
+        :return:
+            Product URI.
+        """
+        if isinstance(in_dir, dict):
+            # the blue band is always available
+            test_band = in_dir['blue']['href'].split('/')[-1]
+        elif isinstance(in_dir, Path):
+            test_band = in_dir.glob("*.TIF")[0]
+        # get the product URI from the band name
+        product_uri = '_'.join(test_band.split('_')[:-2])
+        return product_uri
+
+    @staticmethod
+    def _sensing_time_from_filename(
+            in_dir: Path | Dict[str, str]
+    ) -> datetime:
+        """
+        Get the sensing time from the file name.
+
+        :param in_dir:
+            Path to the directory containing the Landsat data or collection of
+            hyper-links in the case of STAC. If a dictionary is passed, we assume
+            that all files are stored in the same directory.
+        :return:
+            Sensing time.
+        """
+        if isinstance(in_dir, dict):
+            # the blue band is always available
+            test_band = in_dir['blue']['href'].split('/')[-1]
+        elif isinstance(in_dir, Path):
+            test_band = in_dir.glob("*.TIF")[0]
+        # get the sensing time from the band name
+        sensing_time = test_band.split('_')[3]
+        return datetime.strptime(sensing_time, '%Y%m%d')
 
     @staticmethod
     def _sensor_from_platform(
@@ -225,6 +275,7 @@ class Landsat(RasterCollection):
         band_selection: Optional[List[str]] = None,
         read_qa: Optional[bool] = True,
         read_atcor: Optional[bool] = False,
+        apply_scaling: Optional[bool] = True,
         **kwargs
     ):
         """
@@ -247,6 +298,9 @@ class Landsat(RasterCollection):
         :param read_atcor:
             If True, the bands stemming from the atmospheric correction are read
             as well. Default is False.
+        :param apply_scaling:
+            If True, scales reflectance values to the range [0, 1] from original
+            unsigned 16-bit integers. Default is True.
         :param kwargs:
             Additional keyword arguments passed to the `RasterCollection` constructor.
         :return:
@@ -270,7 +324,7 @@ class Landsat(RasterCollection):
         masking_after_read_required = False
 
         if kwargs.get("vector_features") is not None:
-            bounds_df = adopt_vector_features_to_mask(
+            bounds_df, shape_mask, lowest_resolution = adopt_vector_features_to_mask(
                 band_df=band_df,
                 vector_features=kwargs.get("vector_features")
             )
@@ -279,3 +333,85 @@ class Landsat(RasterCollection):
             # update the vector_features entry
             kwargs.update({"vector_features": bounds_df})
 
+        # get platform, sensor and sensing time
+        platform = cls._check_platform(in_dir=in_dir)
+        sensor = cls._sensor_from_platform(platform=platform)
+        sensing_time = cls._sensing_time_from_filename(in_dir=in_dir)
+        # get the product uri
+        product_uri = cls._product_uri_from_filename(in_dir=in_dir)
+
+        # construct the SceneProperties object
+        scene_properties = SceneProperties(
+            platform=f'LANDSAT_{platform[-1]}',
+            sensor=sensor,
+            acquisition_time=sensing_time,
+            product_uri=product_uri,
+            processing_level=ProcessingLevels.L2A  # currently we only support L2A
+        )
+
+        # set proper scaling factors to allow for conversion to
+        # reflectance [0, 1]
+        gain, offset = 0.0001, 0.0
+
+        # loop over bands and add them to the collection of bands
+        landsat = cls(scene_properties=scene_properties)
+        for band_name in list(band_df.band_name):
+            # get entry from dataframe with file-path of band
+            band_safe = band_df[band_df.band_name == band_name]
+            band_fpath = band_safe.band_path.values[0]
+
+            # get alias name of band
+            band_alias = None
+            if band_name in landsat_band_mapping[sensor].values():
+                band_alias = [
+                    k for k, v in landsat_band_mapping[sensor].items()
+                    if v == band_name][0]
+            elif band_name in landsat_band_mapping['quality_flags'].keys():
+                band_alias = band_name.lower()
+            elif band_name in landsat_band_mapping['atmospheric_correction'].keys():
+                band_alias = band_name.lower()
+
+            # read band
+            try:
+                if band_name in landsat_band_mapping[sensor].values():
+                    kwargs.update({'scale': gain, 'offset': offset})
+                landsat.add_band(
+                    Band.from_rasterio,
+                    fpath_raster=band_fpath,
+                    band_idx=1,
+                    band_name_dst=band_name,
+                    band_alias=band_alias,
+                    **kwargs,
+                )
+            except Exception as e:
+                raise Exception(
+                    f"Could not add band {band_name} from {in_dir.name}: {e}"
+                )
+            # apply actual vector features if masking is required
+            if masking_after_read_required:
+                # nothing to do when the lowest resolution is passed
+                if band_safe.band_resolution.values == lowest_resolution:
+                    continue
+                # otherwise resample the mask of the lowest resolution to the
+                # current resolution using nearest neighbor interpolation
+                tmp = shape_mask.astype("uint8")
+                dim_resampled = (landsat[band_name].ncols, landsat[band_name].nrows)
+                res = cv2.resize(
+                    tmp, dim_resampled, interpolation=cv2.INTER_NEAREST_EXACT
+                )
+                # cast back to boolean
+                mask = res.astype("bool")
+                landsat.mask(mask=mask, bands_to_mask=[band_name], inplace=True)
+
+        # scaling of reflectance values (i.e., do not scale SCL)
+        if apply_scaling:
+            sel_bands = landsat.band_names
+            # only scale the reflectance bands
+            sel_bands = [x for x in sel_bands if x in
+                         landsat_band_mapping[sensor].values()]
+            landsat.scale(
+                inplace=True,
+                band_selection=sel_bands,
+                pixel_values_to_ignore=[landsat[landsat.band_names[0]].nodata],
+            )
+        return landsat
