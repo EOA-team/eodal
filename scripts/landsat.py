53fb04c5d4d773d328371ec440c456a1dfc381a4
lukas <lukasvalentin.graf@usys.ethz.ch>
2023-06-02 15:02:42 +0200

    adding documentation

--- scripts/landsat.py
+++ scripts/landsat.py
@@ -1,50 +1,104 @@
+"""
+Script to extract a collection of Landsat scenes for a
+custom area of interest (AOI).
+
+The script shows how to use the EOdal Mapper class that takes over
+data handling such as
+
+* querying of spatio-temporal metadata catalogs to identify
+available Landsat scenes
+* merging data from different tiles if required
+* re-projection of imagery from one UTM zone into another if required
+* removal of black-filled (i.e., no-data) scenes
+
+This script works by retrieving Landsat Collection-2 scenes from
+Microsoft Planetary Computer (https://planetarycomputer.microsoft.com).
+This requires no authentication required. Alternatively, the same code
+can be used to read data from a local EOdal Sentinel archive or a different
+STAC provider.
+
+Copyright (C) 2023 Lukas Valentin Graf
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from datetime import datetime
+from shapely.geometry import box
+
+from eodal.config import get_settings
+from eodal.core.sensors import Landsat
+from eodal.mapper.feature import Feature
+from eodal.mapper.filter import Filter
+from eodal.mapper.mapper import Mapper, MapperConfigs
+
+Settings = get_settings()
+# we use STAC, i.e., Microsoft Planetary Computer
+Settings.USE_STAC = True
+
+
 if __name__ == "__main__":
 
-    # some testing -> to be moved to tests later
-    import geopandas as gpd
-
-    from datetime import datetime
-    from eodal.core.sensors import Landsat
-    from eodal.mapper.feature import Feature
-    from eodal.mapper.filter import Filter
-    from eodal.metadata.stac.client import landsat
-    from shapely.geometry import box
-
-    # query STAC for a custom region
+    # user-inputs
+    # -------------------------- Collection -------------------------------
     collection = 'landsat-c2-l2'
-    bbox = box(*[7.0, 47.0, 8.0, 48.0])
+
+    # ---------------------- Spatial Feature  ------------------------------
+    bbox = box(*[7.0, 47.0, 8.0, 48.0])  # can be also shp, gpkg, etc.
     feature = Feature(
         name='landsat-test',
         geometry=bbox,
         epsg=4326,
         attributes={})
+
+    # ------------------------- Time Range ---------------------------------
     time_start = datetime(2023, 5, 1)
     time_end = datetime(2023, 5, 30)
 
+    # ------------------------- Metadata Filters ---------------------------
     metadata_filters = [
         Filter('eo:cloud_cover', '<', 70),
-        Filter('instruments', '==', 'oli')
+        Filter('landsat:wrs_row', '==', '027')
     ]
 
-    landsat_items = landsat(
+    # set up the Mapper configuration
+    mapper_configs = MapperConfigs(
         metadata_filters=metadata_filters,
         collection='landsat-c2-l2',
-        bounding_box=bbox,
+        feature=feature,
         time_start=time_start,
         time_end=time_end)
+    # get a new mapper instance
+    mapper = Mapper(mapper_configs)
 
-    # read only a part of the test scene
-    landsat_scene_item = landsat_items.iloc[0]
-    gdf = gpd.GeoSeries([bbox], crs=4326)
-    landsat = Landsat.from_usgs(
-        in_dir=landsat_scene_item['assets'],
-        vector_features=gdf,
-        read_qa=False,
-        read_atcor=False
-    )
+    # query the scenes available (no I/O of scenes, this only fetches metadata)
+    mapper.query_scenes()
+    # the metadata is stored as a GeoDataFrame
+    mapper.metadata
 
-    # get a complete test scene
-    landsat = Landsat.from_usgs(
-        in_dir=landsat_scene_item['assets'],
-        band_selection=['blue', 'green', 'red']
-    )
+    # we tell EOdal how to load the Landsat scenes using `Landsat.from_usgs`
+    # and pass on some kwargs, e.g., the selection of bands we want to read.
+    scene_kwargs = {
+        'scene_constructor': Landsat.from_usgs,
+        'scene_constructor_kwargs': {'band_selection':
+                                     ["blue", "green", "red", "nir08"]}}
+
+    # now we load the scenes
+    mapper.load_scenes(scene_kwargs=scene_kwargs)
+    # the scenes are loaded into a EOdal SceneCollection object
+    mapper.data
+
+    # the scenes can be plotted
+    f_scenes = mapper.data.plot(['red', 'green', 'blue'], figsize=(15,5))
+
+    # use mapper.data to work with EOdal SceneCollections ...
