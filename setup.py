302539b29dd1cf11cd30b17249241991d5bdf5d8
lukas <lukasvalentin.graf@usys.ethz.ch>
2023-06-05 12:41:01 +0200

    adding support for Python 3.6, 3.7 and 3.11

--- setup.py
+++ setup.py
@@ -158,9 +158,12 @@ setup(
      "Natural Language :: English",
      "Programming Language :: Python :: 3",
      "Operating System :: OS Independent",
+     "Programming Language :: Python :: 3.6",
+     "Programming Language :: Python :: 3.7",
      "Programming Language :: Python :: 3.8",
      "Programming Language :: Python :: 3.9",
-     "Programming Language :: Python :: 3.10"
+     "Programming Language :: Python :: 3.10",
+     "Programming Language :: Python :: 3.11",
     ],
     # Could also include keywords, download_url, project_urls, etc.
     # Custom commands
