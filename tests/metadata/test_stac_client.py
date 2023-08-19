78372e259a77800d9f3fe35441983b0209d314fb
lukas <lukasvalentin.graf@usys.ethz.ch>
2023-08-19 15:04:30 +0200

    fixed broken test

--- tests/metadata/test_stac_client.py
+++ tests/metadata/test_stac_client.py
@@ -3,6 +3,7 @@ Tests for the pystac client interface
 '''
 
 import pytest
+import geopandas as gpd
 
 from datetime import datetime
 from shapely.geometry import box
@@ -19,14 +20,14 @@ def test_mspc_sentinel1(get_polygons):
     time_start = datetime(2022, 5, 1)
     time_end = datetime(2022, 5, 31)
 
-    polys = get_polygons()
-    bbox = box(*polys.to_crs(epsg=4326).total_bounds())
+    polys = gpd.read_file(get_polygons())
+    bbox = box(*polys.to_crs(epsg=4326).total_bounds)
 
     # test RTC product
     metadata_filters = [Filter('product_type', '==', 'RTC')]
     res_s1 = sentinel1(
         metadata_filters=metadata_filters,
-            collection='sentinel1-grd',
+            collection='sentinel1-rtc',
             bounding_box=bbox,
             time_start=time_start,
             time_end=time_end
@@ -39,6 +40,7 @@ def test_mspc_sentinel1(get_polygons):
     assert 'rtc' in res_s1.iloc[0].assets['vh']['href'], 'RTC not found in file name'
 
     # test GRD
+    metadata_filters = [Filter('product_type', '==', 'GRD')]
     res_grd_s1 = sentinel1(
         metadata_filters=metadata_filters,
             collection='sentinel1-grd',
@@ -64,12 +66,12 @@ def test_mspc_sentinel2(get_polygons):
     # set scene cloud cover threshold [%]
     cloud_cover_threshold = 80
 
-    polys = get_polygons()
-    bbox = box(*polys.to_crs(epsg=4326).total_bounds())
+    polys = gpd.read_file(get_polygons())
+    bbox = box(*polys.to_crs(epsg=4326).total_bounds)
 
     metadata_filters = [
         Filter('cloudy_pixel_percentage', '<', cloud_cover_threshold),
-        Filter('processing_level', '==', 'L2A')
+        Filter('processing_level', '==', 'Level-2A')
     ]
 
     # run stack query and make sure some items are returned
