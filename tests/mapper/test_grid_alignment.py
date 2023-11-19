04d944554c309dfa115b55a813e4066c50dfc94e
lukas <lukasvalentin.graf@usys.ethz.ch>
2023-11-19 16:27:11 +0100

    moved `USE_STAC` statement inside test function

--- tests/mapper/test_grid_alignment.py
+++ tests/mapper/test_grid_alignment.py
@@ -16,10 +16,13 @@ from eodal.mapper.filter import Filter
 from eodal.mapper.mapper import Mapper, MapperConfigs
 
 settings = get_settings()
-settings.USE_STAC = True
 
 
 def test_grid_alignment(get_polygons_utm_zones):
+    """test grid alignment for Sentinel-2"""
+
+    settings.USE_STAC = True
+
     time_start = datetime(2018, 3, 2)
     time_end = datetime(2018, 4, 6)
     metadata_filters = [
@@ -70,7 +73,9 @@ def test_grid_alignment(get_polygons_utm_zones):
             ncols.append(band.ncols)
 
     assert len(set(pixres_x)) == 1
+    assert pixres_x[0] == 10
     assert len(set(pixres_y)) == 1
+    assert pixres_y[0] == -10
     assert len(set(ulx)) == 1
     assert len(set(uly)) == 1
     assert len(set(nrows)) == 1
@@ -108,6 +113,14 @@ def test_grid_alignment(get_polygons_utm_zones):
         assert len(set(pixres_y)) == 1
         assert len(set(nrows)) == 1
         assert len(set(ncols)) == 1
+        # only the red band should have a spatial resolution of 10 m
+        # as we did not resample the 20 m bands
+        if band_name == 'B04':
+            assert pixres_x[0] == 10.
+            assert pixres_y[0] == -10.
+        else:
+            assert pixres_x[0] == 20.
+            assert pixres_y[0] == -20.
 
     assert len(set(ulx)) == 1
     assert len(set(uly)) == 1
